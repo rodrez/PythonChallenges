5e8820df6f8761b90a34697de27a367d1155eae5
Fabian Rodrz'git config --global user.name Fabian <fjrodriguez6419@gmail.com>
2020-01-07 20:39:35 -0500

    Guess the Number - First Release
    This version will ask the user to guess a number from 1 to 20 and it will let the user know if their number is higher or lower than the number they are trying to guess.

---
+++ guess_the_number.py
@@ -0,0 +1,19 @@
+import random
+
+print('Welcome to Guess the number game!')
+print('Try to guess a random number from 1 to 20')
+
+# Use the random library to decide a random number between 1-20
+random_number = random.randint(1, 20)
+
+# Game Logic
+player_choice = ''
+while player_choice != random_number:
+    player_choice = int(input('Select a number from 1-20\n'))
+    if player_choice > random_number:
+        print('Your number is greater than the random number!')
+    elif player_choice < random_number:
+        print('Your number is less than the random number.')
+    elif player_choice == random_number:
+        print('Congratulations you guess the number!')
+        break
