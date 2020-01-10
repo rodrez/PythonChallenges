fdcb38e73ae36f11b1a0e8b0128915adb81d93e9
Fabian <fabian.rodrez@gmail.com>
2020-01-09 22:17:23 -0500

    -Added Turns have been added to the game. Letting the player now the remaining turns if the player selects an incorrect letter. -Added 'You have been hanged' if the player loses the game. [Next item to work on is to request a new letter if a letter has been used already]
    Signed-off-by: Fabian <fabian.rodrez@gmail.com>

--- hang.py
+++ hang.py
@@ -9,23 +9,32 @@ secret_word = ['_'] * len(word)
 
 print(secret_word)
 
-# Keeps the game running until the break happens
-while True:
-    chosen_letter = input('Enter a letter from A to Z:')
-    used_letters = []
-    failed_attempts = 0
 
+# def replay():
+#   return input('Would you like to play another game?').lower().startswith('y')
+
+
+# Keeps the game running until the break happens
+failed_attempts = 0
+used_letters = []
+
+while failed_attempts != 6:
+    chosen_letter = input('Enter a letter from A to Z:')
     # Use enumerate to iterate trough the word and get the index position and the letter
     for i, char in enumerate(word):
         if chosen_letter == char:
             secret_word[i] = char
         # Adds the chosen letter to the list that way the user can't repeat the same letters
-        # TODO: used_letters.append(chosen_letter)
+            used_letters.append(chosen_letter)
             print(f'A new letter was added to the secret word \n {secret_word}')
-
     # Check is the user won the game!
-    if ''.join(secret_word) == word or failed_attempts == 6:
+    if ''.join(secret_word) == word:
         break
     elif chosen_letter not in word:
         failed_attempts += 1
+        used_letters.append(chosen_letter)
         print(f'The amount of turns left is {6 - failed_attempts}')
+
+else:
+    print("You have been hanged!")
+
