b8c49d8847df2d9ece38c3eb556ef741e0bb903d
Fabian <fabian.rodrez@gmail.com>
2020-01-28 22:24:17 -0500

    This code is for another challenge I came across. The purpose of the code is to create a list with only numbers.
    Signed-off-by: Fabian <fabian.rodrez@gmail.com>

--- filter_list.py
+++ filter_list.py
@@ -1,14 +1,10 @@
-filtered_list = []
-
-
-# This function will remove anything that is not an integer, and every integer will be assigned to a new list.
 def filter_list(l):
-    for item in l:
-        if type(item) == int:
-            filtered_list.append(item)
+    filtered_list = [item for item in l if type(item) == int]
+    # for item in l:
+    #     if type(item) == int:
+    #         filtered_list.append(item)
     print(filtered_list)
 
 filter_list([1, 2, 'a', 'b'])
 filter_list([1,'a','b',0,15])
 filter_list([1,2,'aasf','1','123',123])
-
