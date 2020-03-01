c2a2a2485db1548ab24a89cdc5daae4e134951ef
Rodrez <fabian.rodrez@gmail.com>
2020-02-29 23:43:41 -0500

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-# Palindrome
-
-word = 'racecar'
-reversed_word = word[::-1]
-
-if word == reversed_word:
-      print('The word is a Palindrome')
-else:
-      print('The word is not a Palindrome.')
